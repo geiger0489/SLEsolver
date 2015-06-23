bde73979a88cba914efe9b5b7f5b3bc56553c893
Alexander <sashageiger@gmail.com>
2015-06-23 16:01:57 +0300

    Updated Matrix class
    1. Updated Matrix class
    2. Revised tests of Matrix class

--- matrix.py
+++ matrix.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 class Matrix():
-    def __init__(self, m = None):
+    def __init__(self, m = []):
         self.m = m
 
     def __str__(self):
@@ -15,6 +15,45 @@ class Matrix():
             return s
         else:
             return 'Empty matrix'
+        
+    def null(self, r, c):
+        '''
+        Generates a null-matrix of size R x C.
+        '''
+    
+        if r < 0 or c < 0 or not type(r) == int or not type(c) == int:
+            raise ValueError('Rows and cols must be positive integers.')
+        
+        self.m = [[0 for i in range(c)] for j in range(r)]
+        
+    def unit(self, n):
+        '''
+        Generates a square unit-matrix of size N x N.
+        '''
+        
+        self.null(n, n)
+        for i in range(n):
+            self.m[i][i] = 1
+    
+
 
 if __name__=='__main__':
-    pass
+    import unittest
+    
+    class MatrixTestCase(unittest.TestCase):
+    
+        def test_print_matrix(self):
+            m = Matrix([[1,2], [3,4]])
+            self.assertEqual(m.__str__(), '| 1 2 |\n| 3 4 |\n')
+    
+        def test_create_null_matrix(self):
+            m = Matrix()
+            m.null(2, 3)
+            self.assertEqual(m.m, [[0, 0, 0],[0, 0, 0]])
+            
+        def test_create_unit_matrix(self):
+            m = Matrix()
+            m.unit(3)
+            self.assertEqual(m.m, [[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+        
+    unittest.main()
