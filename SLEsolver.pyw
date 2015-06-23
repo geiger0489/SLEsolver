c89d2565b060d61e20f7a03c71c2c332049f3ec7
Alexander <sashageiger@gmail.com>
2015-06-23 18:42:12 +0300

    Extended Matrix class
    1. Added invert method
    2. Added dimensions method
    3. Added is_square method
    4. Added set method
    5. Added __add__, __sub__ methods
    6. Revised tests

--- SLEsolver.pyw
+++ SLEsolver.pyw
@@ -8,8 +8,6 @@ import core
 APP_NAME = 'SLE solver'
 VERSION = '1.0'
 
-icon = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABmElEQVQ4T6WTPWvCUBiFjx9xciqIIF0Esbh06y9wclAQRVxFouLk0LVD/4DgT2h3g0OKXxE6iGJ0Sqe2QxCqdjFU1IIotdzXxmildEjgwrk3933ec25yLTD5WABszTAIcC9I6D+9EOtZGeDi8uoPJuvFSgDu6xPF22uabW+KdxAf+wTQ3l5xdu7/F/DxrkKVH3aASqWC6XQKm80GWZaRSqVgt9uPBsdxR3NJkpBMJg2Apmm0odfrIZ1OnwB0oA5qNptIJBI7QLlcxmKxIAeHANalUChgOByeABkgHo8bDubzOW3qdrvgeZ50IBCAqqqkf0eo1+uIxWIGYLlckgMGyGQyVOD37w5zPB7D4/GQFgQBoVAItVoN0WjUiLBerwnQ6XSQzWapq8/nw2g0Iu12u9FqtRAMBrFardBoNBAOhw0HbFF3kMvlqMjr9WIymZB2uVx0Tk6nE5vNBtVq9RjAFhmg3W4jn8+fZD78rCyeKIoGoFQqYTabUUZFURCJRGC1WmkwqK7Ze4fDsf/J9hHM3gUz9T83wwTiG3cQsgR+G17SAAAAAElFTkSuQmCC'
-
 class MainWindow(QtGui.QWidget):
     def __init__(self, parent = None):
      super(MainWindow, self).__init__(parent)
--- matrix.py
+++ matrix.py
@@ -2,7 +2,7 @@
 
 class Matrix():
     def __init__(self, m = []):
-        self.m = m
+        self.set(m)
 
     def __str__(self):
         if self.m:
@@ -15,45 +15,132 @@ class Matrix():
             return s
         else:
             return 'Empty matrix'
+            
+    def __add__(self, other):
+        '''
+        Sums two equal matrices. A + B = C.
+        '''
+        
+        if not other.dimensions() == self.dimensions():
+            raise Exception('Matrix dimensions must be equal.')
+            
+        for i in range( len(self.m) ):
+            for j in range( len(self.m[i]) ):
+                self.m[i][j] += other.m[i][j]
+        
+        return self
+        
+    def __sub__(self, other):
+        '''
+        Subtracts two equal matrices. A - B = C.
+        '''
+        
+        other.invert()
+        return self.__add__(other)
+    
+    def set(self, m):
+        if not type(m) == list:
+            raise TypeError('Input parameter must be of type "list".')
+        
+        self.m = m
+    
+    def is_empty(self):
+        if self.m:
+            return False
+        else:
+            return True
         
     def null(self, r, c):
         '''
         Generates a null-matrix of size R x C.
         '''
     
-        if r < 0 or c < 0 or not type(r) == int or not type(c) == int:
+        if r < 0 or c < 0 or not type(r) == type(c) == int:
             raise ValueError('Rows and cols must be positive integers.')
         
         self.m = [[0 for i in range(c)] for j in range(r)]
         
-    def unit(self, n):
+    def unity(self, n):
         '''
-        Generates a square unit-matrix of size N x N.
+        Generates a square unity matrix of size N x N.
         '''
         
         self.null(n, n)
         for i in range(n):
             self.m[i][i] = 1
+            
+    def invert(self):
+        '''
+        Inverts the matrix (aij = -aij)
+        '''
+        
+        for i in range( len(self.m) ):
+            for j in range( len(self.m[i]) ):
+                self.m[i][j] *= -1
+                
+    def dimensions(self):
+        '''
+        Returns matrix dimensions as a tuple (rows, cols).
+        '''
+        if not self.m:
+            raise Exception('Trying to get dimensions of empty matrix')
+        return (len(self.m), len(self.m[0]))
+        
+    def is_square(self):
+        '''
+        Returns True if the matrix is square
+        and returns False if it's not.
+        '''
+        
+        if not self.m:
+            raise Exception('Matrix is empty.')
+        return len(self.m) == len(self.m[0])
     
-
-
 if __name__=='__main__':
     import unittest
     
     class MatrixTestCase(unittest.TestCase):
     
         def test_print_matrix(self):
-            m = Matrix([[1,2], [3,4]])
-            self.assertEqual(m.__str__(), '| 1 2 |\n| 3 4 |\n')
+            m = Matrix([[1, 2, 3], [4, 5, 6]])
+            self.assertEqual(m.__str__(), '| 1 2 3 |\n| 4 5 6 |\n')
     
         def test_create_null_matrix(self):
             m = Matrix()
             m.null(2, 3)
             self.assertEqual(m.m, [[0, 0, 0],[0, 0, 0]])
             
-        def test_create_unit_matrix(self):
+        def test_create_unity_matrix(self):
             m = Matrix()
-            m.unit(3)
+            m.unity(3)
             self.assertEqual(m.m, [[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+            
+        def test_invert_matrix(self):
+            m = Matrix()
+            m.unity(3)
+            m.invert()
+            self.assertEqual(m.m, [[-1, 0, 0], [0, -1, 0], [0, 0, -1]])
+            
+        def test_get_matrix_dimensions(self):
+            m = Matrix([[-1, 0, 0], [0, 0, -1]])
+            self.assertEqual(m.dimensions(), (2, 3))
+
+        def test_sum_two_matrices(self):
+            m1 = Matrix([[1, 1], [1, 1]])
+            m2 = Matrix([[1, 1], [1, 1]])
+            m3 = m1 + m2
+            self.assertEqual(m3.m, [[2, 2], [2, 2]])
         
+        def test_subtract_two_matrices(self):
+            m1 = Matrix([[1, 1], [1, 1]])
+            m2 = Matrix([[1, 1], [1, 1]])
+            m3 = m1 - m2
+            self.assertEqual(m3.m, [[0, 0], [0, 0]])
+            
+        def test_matrix_is_square(self):
+            m1 = Matrix([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
+            m2 = Matrix([[1, 1, 1], [1, 1, 1]])
+            self.assertTrue(m1.is_square())
+            self.assertFalse(m2.is_square())
+            
     unittest.main()
