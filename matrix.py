5b9d8f9b4fe71ecb8997836eb812571caac81e73
geiger0489 <sashageiger@gmail.com>
2015-06-23 22:05:33 +0300

    More extensions of Matrix class
    1. Added transpose method
    2. Added fill method
    3. Added ones and null methods

--- matrix.py
+++ matrix.py
@@ -50,15 +50,29 @@ class Matrix():
         else:
             return True
         
-    def null(self, r, c):
+    def fill(self, r, c, n):
         '''
-        Generates a null-matrix of size R x C.
+        Generates a matrix of size R x C with all elements equal to n.
         '''
     
         if r < 0 or c < 0 or not type(r) == type(c) == int:
             raise ValueError('Rows and cols must be positive integers.')
         
-        self.m = [[0 for i in range(c)] for j in range(r)]
+        self.m = [[n for i in range(c)] for j in range(r)]
+
+    def null(self, r, c):
+        '''
+        Generates a null-matrix of size R x C.
+        '''
+        
+        self.fill(r, c, 0)
+        
+    def ones(self, r, c):
+        '''
+        Generates a all-ones matrix of size R x C.
+        '''
+        
+        self.fill(r, c, 1)
         
     def unity(self, n):
         '''
@@ -82,6 +96,7 @@ class Matrix():
         '''
         Returns matrix dimensions as a tuple (rows, cols).
         '''
+        
         if not self.m:
             raise Exception('Trying to get dimensions of empty matrix')
         return (len(self.m), len(self.m[0]))
@@ -92,10 +107,24 @@ class Matrix():
         and returns False if it's not.
         '''
         
-        if not self.m:
-            raise Exception('Matrix is empty.')
         return len(self.m) == len(self.m[0])
-    
+        
+    def transpose(self):
+        '''
+        Transposes a given matrix. a[i][j] --> a[j][i] 
+        '''
+        
+        r, c = self.dimensions()
+        
+        if r == c: # if the matrix is square
+            for i in range(r):
+                for j in range(i, c):
+                    if i == j:
+                        continue
+                    b = self.m[i][j]
+                    self.m[i][j] = self.m[j][i]
+                    self.m[j][i] = b
+                      
 if __name__=='__main__':
     import unittest
     
@@ -104,11 +133,21 @@ if __name__=='__main__':
         def test_print_matrix(self):
             m = Matrix([[1, 2, 3], [4, 5, 6]])
             self.assertEqual(m.__str__(), '| 1 2 3 |\n| 4 5 6 |\n')
-    
+
+        def test_fill_matrix_with_n(self):
+            m = Matrix()
+            m.fill(2, 3, 5)
+            self.assertEqual(m.m, [[5, 5, 5],[5, 5, 5]])
+            
         def test_create_null_matrix(self):
             m = Matrix()
             m.null(2, 3)
             self.assertEqual(m.m, [[0, 0, 0],[0, 0, 0]])
+            
+        def test_create_ones_matrix(self):
+            m = Matrix()
+            m.ones(2, 3)
+            self.assertEqual(m.m, [[1, 1, 1],[1, 1, 1]])
             
         def test_create_unity_matrix(self):
             m = Matrix()
@@ -142,5 +181,10 @@ if __name__=='__main__':
             m2 = Matrix([[1, 1, 1], [1, 1, 1]])
             self.assertTrue(m1.is_square())
             self.assertFalse(m2.is_square())
+        
+        def test_transpose_matrix(self):
+            m = Matrix([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
+            m.transpose()
+            self.assertEqual(m.m, [[1, 1, 1], [2, 2, 2], [3, 3, 3]])
             
     unittest.main()
